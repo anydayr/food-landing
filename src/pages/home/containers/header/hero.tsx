5473084a9f009300d3241fecfb6c385ee26d3cff
anydayr <annyrondon491@gmail.com>
2023-12-05 15:32:20 -0400

    refactor: replace background image to image tag

--- src/pages/home/containers/header/hero.tsx
+++ src/pages/home/containers/header/hero.tsx
@@ -1,6 +1,13 @@
+import BackgroundHeader from '../../../../assets/images/background-hero.png'
 function Hero() {
   return (
     <header className="header__container">
+      <img
+        src={BackgroundHeader}
+        alt="Hero image"
+        className="header__img"
+        loading="lazy"
+      />
       <h1 className="header__title h1">El secreto de tu cocina</h1>
     </header>
   )
